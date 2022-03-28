7c5580750c42ad968fb36e181618d8d9eb24430a
AddFluotine <2045520328@qq.com>
2022-03-28 13:24:46 +0800

    first initialization

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+/*
+ * @Author: Ue
+ * @Date: 2022-03-28 13:21:23
+ * @LastEditTime: 2022-03-28 13:21:23
+ * @LastEditors: Ue
+ * @FilePath: /work-space/react-admin-client/src/App.jsx
+ */
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return <div>App</div>;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*
+ * @Author: Ue
+ * @Date: 2022-03-28 13:17:51
+ * @LastEditTime: 2022-03-28 13:21:05
+ * @LastEditors: Ue
+ * @FilePath: /work-space/react-admin-client/src/index.js
+ */
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import App from "./App";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById("root"));
